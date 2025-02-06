6bb487265a3ee91201dee9627aa53a488fb11a5a
Dmorel <98861502+DiegoMorel@users.noreply.github.com>
2025-02-06 18:22:43 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = new int[10];
+
+        // Pedir al usuario que ingrese 10 números
+        Console.WriteLine("Por favor, ingresa 10 números desordenados:");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Ingresa el número {i + 1}: ");
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Ordenar el arreglo de números
+        Array.Sort(numeros);
+
+        // Mostrar los números ordenados
+        Console.WriteLine("Los números ordenados son:");
+        foreach (int numero in numeros)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
